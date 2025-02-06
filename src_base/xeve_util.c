b7326b12af321b493a676d7506e1a7443697b5cf
Neeraj Gadgil <neergil@gmail.com>
2025-02-06 16:15:37 +0530

    Fixed edge cases for neighbour availability in intra-pred

--- src_base/xeve_util.c
+++ src_base/xeve_util.c
@@ -720,8 +720,8 @@ u16 xeve_get_avail_intra(int x_scu, int y_scu, int w_scu, int h_scu, int scup, i
     {
         SET_AVAIL(avail, AVAIL_LE);
 
-        if(y_scu + scuh + scuw - 1 < h_scu  && MCU_GET_COD(map_scu[scup + (w_scu * (scuw + scuh)) - w_scu - 1]) &&
-           (map_tidx[curr_scup] == map_tidx[scup + (w_scu * (scuw + scuh)) - w_scu - 1]))
+        if (y_scu + scuh < h_scu && MCU_GET_COD(map_scu[scup + (w_scu * scuh) - 1]) &&
+            (map_tidx[curr_scup] == map_tidx[scup + (w_scu * scuh) - 1]))
         {
             SET_AVAIL(avail, AVAIL_LO_LE);
         }
@@ -753,8 +753,8 @@ u16 xeve_get_avail_intra(int x_scu, int y_scu, int w_scu, int h_scu, int scup, i
     {
         SET_AVAIL(avail, AVAIL_RI);
 
-        if(y_scu + scuh + scuw - 1 < h_scu  && MCU_GET_COD(map_scu[scup + (w_scu * (scuw + scuh - 1)) + scuw]) &&
-           (map_tidx[curr_scup] == map_tidx[scup + (w_scu * (scuw + scuh - 1)) + scuw]))
+        if (y_scu + scuh < h_scu && MCU_GET_COD(map_scu[scup + (w_scu * scuh) + scuw]) &&
+            (map_tidx[curr_scup] == map_tidx[scup + (w_scu * scuh) + scuw]))
         {
             SET_AVAIL(avail, AVAIL_LO_RI);
         }
